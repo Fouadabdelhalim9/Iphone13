4a7ea1d58e7f1baec48d1069ccf336bedee52b00
FouadAbdelhalim <121126454+Fouadabdelhalim9@users.noreply.github.com>
2023-03-25 18:37:01 +0200

    Add files via upload

---
+++ main/main.js
@@ -0,0 +1,9 @@
+ let image=document.querySelector('.photo');
+ let container=document.querySelector('.container');
+ function phones(x){
+    image.src= x;
+ }
+ 
+function colors(color){
+   container.style.background= color;
+}
